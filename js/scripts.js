722d372521401a3bddd55c9e1c107a2d65d9e100
Guest User <student@epicodus.com>
2015-05-28 15:15:34 -0700

    Add hexadecimal converter. Pass spec.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,42 @@
-var base = function(binaryString) {
+var binaryConverter = function(binaryString) {
   var binaryArray = binaryString.split("").reverse();
   var decimalNum = 0;
   for(var i = 0; i < binaryArray.length; i++) {
-    if(binaryArray[i] == "1") {
-      decimalNum += Math.pow(2, i);
-    }
+    decimalNum += Math.pow(2, i) * parseInt(binaryArray[i]);
   }
   return decimalNum;
 }
+
+var trinaryConverter = function(trinaryString) {
+  var trinaryArray = trinaryString.split("").reverse();
+  var decimalNum = 0;
+  for(var i = 0; i < trinaryArray.length; i++) {
+    decimalNum += Math.pow(3, i) * parseInt(trinaryArray[i]);
+  }
+  return decimalNum;
+}
+
+var upToBaseTenConverter = function(baseString, base) {
+  var baseArray = baseString.split("").reverse();
+  var decimalNum = 0;
+  for(var i = 0; i < baseArray.length; i++) {
+      decimalNum += Math.pow(base, i) * parseInt(baseArray[i]);
+  }
+  return decimalNum;
+}
+
+var hexadecimalConverter = function(input) {
+  var charLookup = {"1": 1,"2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "a": 10, "b": 11, "c": 12, "d": 13, "e": 14, "f": 15, "g": 16}
+  var hexArray = input.split("").reverse();
+  var decimalNum = 0;
+  for(var i = 0; i < hexArray.length; i++) {
+    decimalNum += Math.pow(16, i) * charLookup[hexArray[i]];
+  }
+  return decimalNum;
+}
+
+
+// var characterToNumber = function(character) {
+//   outputNumber = character
+//   characterLookup = {"1": 1,"2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "a": 10, "b": 11, "c": 12, "d": 13, "e": 14, "f": 15, "g": 16}
+// }
--- spec/specs.js
+++ spec/specs.js
@@ -1,9 +1,31 @@
-describe("base", function() {
+describe("binaryConverter", function() {
   it("will return the given binary string as a decimal number", function() {
-    expect(base("10011100010000")).to.equal(10000);
+    expect(binaryConverter("10011100010000")).to.equal(10000);
   });
 
   it("will return the given binary string as a decimal number", function() {
-    expect(base("10000000011110")).to.equal(8222);
+    expect(binaryConverter("10000000011110")).to.equal(8222);
   });
+});
+
+describe("trinaryConverter", function() {
+  it("will return the given trinary string as a decimal number", function() {
+    expect(trinaryConverter("1001")).to.equal(28);
+  });
+
+  it("will return the given trinary string as a decimal number", function() {
+    expect(trinaryConverter("20112")).to.equal(176);
+  });
+});
+
+describe("upToBaseTenConverter", function() {
+  it("will return the given string in any base under 11 as a decimal number", function() {
+    expect(upToBaseTenConverter("1322", 4)).to.equal(122);
+  })
+});
+
+describe("hexadecimalConverter", function() {
+  it("will return the given hexadecimal string as a decimal number", function() {
+    expect(hexadecimalConverter("73f")).to.equal(1855);
+  })
 });
