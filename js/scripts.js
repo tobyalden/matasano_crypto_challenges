26a8a49709e08d2fb0fc080a1223e0b775802ec9
Toby Alden <toby.alden@gmail.com>
2015-08-02 17:57:35 -0700

    Working commit.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,28 @@
-
-var charLookup = {"0": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "a": 10, "b": 11, "c": 12, "d": 13, "e": 14, "f": 15, "g": 16}
+BigNumber.config({ DECIMAL_PLACES: 1e+9 })
+BigNumber.config({ POW_PRECISION: 1e+9 })
+var hexChars = {"0": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "a": 10, "b": 11, "c": 12, "d": 13, "e": 14, "f": 15, "g": 16}
 
 var baseConverter = function(input, base) {
   input = input.toLowerCase()
   var baseArray = input.split("").reverse();
-  var decimalNum = 0;
+  var decimalNum = new BigNumber(0);
   for(var i = 0; i < baseArray.length; i++) {
-    decimalNum += Math.pow(base, i) * charLookup[baseArray[i]];
+    var increment = new BigNumber(base);
+    increment = increment.toPower(i).times(hexChars[baseArray[i]]);
+    decimalNum = decimalNum.plus(increment);
   }
   return decimalNum;
 }
 
 var hexToBase64 = function(hexString) {
-  debugger;
-  decimalNum = baseConverter(hexString, 16);
+  var decimalNum = baseConverter(hexString, 16);
   var i = 0;
-  while(Math.pow(64, i) < decimalNum) {
+  var place = new BigNumber(64);
+  place = place.toPower(i);
+  while(decimalNum.greaterThan(place)) {
     i++;
+    place = new BigNumber(64);
+    place = place.toPower(i);
   }
   if(i > 0) {
     i--;
@@ -25,11 +31,12 @@ var hexToBase64 = function(hexString) {
   var base64String = "";
 
   while(i >= 0) {
-    debugger;
-    integerQuotient = Math.floor(decimalNum / Math.pow(64, i))
-    remainder = decimalNum%(Math.pow(64, i));
-    base64String = base64String.concat(getBase64Character(integerQuotient));
-    decimalNum -= Math.pow(64, i) * integerQuotient;
+    place = new BigNumber(64);
+    place = place.toPower(i);
+    var integerQuotient = decimalNum.dividedToIntegerBy(place);
+    base64String = base64String.concat(getBase64Character(integerQuotient.toNumber()));
+    var decrement = place.times(integerQuotient);
+    decimalNum = decimalNum.minus(decrement);
     i--;
   }
 
@@ -37,13 +44,12 @@ var hexToBase64 = function(hexString) {
 
 }
 
-var base64Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0987654321+/";
+var base64Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 var getBase64Character = function(decimalNum) {
   return base64Alphabet.split("")[decimalNum];
 }
 
 $(document).ready(function() {
-
   $("#base-converter").submit(function(event) {
     event.preventDefault();
     var hex = $("input#input-number").val();
