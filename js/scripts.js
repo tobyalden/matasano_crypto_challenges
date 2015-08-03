e0cd75595539afd67f4286760dd25c7dbe3e6174
Toby Alden <toby.alden@gmail.com>
2015-08-02 18:06:05 -0700

    Cleaned up.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 BigNumber.config({ DECIMAL_PLACES: 1e+9 })
 BigNumber.config({ POW_PRECISION: 1e+9 })
-var hexChars = {"0": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "a": 10, "b": 11, "c": 12, "d": 13, "e": 14, "f": 15, "g": 16}
 
+var hexChars = {"0": 0, "1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "a": 10, "b": 11, "c": 12, "d": 13, "e": 14, "f": 15, "g": 16}
 var baseConverter = function(input, base) {
   input = input.toLowerCase()
   var baseArray = input.split("").reverse();
@@ -49,10 +49,11 @@ var getBase64Character = function(decimalNum) {
   return base64Alphabet.split("")[decimalNum];
 }
 
+
 $(document).ready(function() {
-  $("#base-converter").submit(function(event) {
+  $("#hex-to-base64").submit(function(event) {
     event.preventDefault();
-    var hex = $("input#input-number").val();
+    var hex = $("#hex-to-base64 #input").val();
     var output = hexToBase64(hex);
     $(".output").text(output);
   });
